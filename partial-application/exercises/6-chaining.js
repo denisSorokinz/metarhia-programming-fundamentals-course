2e68c316869f39f0ab2cbdb08d97ce4db372cdd0
denisSorokinZ <d.sorokin@gmail.com>
2023-10-17 14:15:06 +0300

    currying

--- partial-application/exercises/6-chaining.js
+++ partial-application/exercises/6-chaining.js
@@ -1,8 +1,12 @@
-'use strict';
+"use strict";
 
 // Check 4 digit pin.
-const EXPECTED_PIN = '4967';
-const checkPin = (...code) => code.join('') === EXPECTED_PIN;
+const PIN_LENGTH = 4;
+const EXPECTED_PIN = "4967";
+const checkPin = (...code) => {
+  console.log(code, code.join("") === EXPECTED_PIN);
+  return code.join("") === EXPECTED_PIN;
+};
 
 // Implement function press
 // that allows to enter pin code by one character,
@@ -10,6 +14,18 @@ const checkPin = (...code) => code.join('') === EXPECTED_PIN;
 //
 // For hint use https://github.com/HowProgrammingWorks/Cheatsheet
 
-const press = null;
+const press = (digit) => {
+  const entered = [digit];
+
+  return {
+    press(dgt) {
+      entered.push(dgt);
+
+      return entered.length < PIN_LENGTH ? this : checkPin(...entered);
+    },
+  };
+};
+
+console.log(press("4").press("9").press("6").press("7"));
 
 module.exports = { press };
